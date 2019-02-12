e98a4bb1b62c03b95ef14365ea1215b3be635648
Kevin Smith <keveightysev@gmail.com>
2019-02-12 14:12:17 -0600

    Created display and number button component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const Display = (props) => {
+    return (
+        <div className="display">
+            <p className="display-text">{props.number}</p>
+        </div>
+    );
+}
+
+export default Display;
