1476113c58ab2265ae091def672e501267392daf
Kevin Smith <keveightysev@gmail.com>
2019-02-12 15:06:08 -0600

    Styled button components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -8,17 +8,18 @@ const App = () => {
   return (
     <div className="calculator">
       <Display number="0" />
+      <div className="buttons">
       <div className="number-buttons">
         <ActionButton buttonStyle="large-button" text="clear" />
-        <NumberButton buttonStyle="small-button" text="7" />
-        <NumberButton buttonStyle="small-button" text="8" />
-        <NumberButton buttonStyle="small-button" text="9" />
-        <NumberButton buttonStyle="small-button" text="4" />
-        <NumberButton buttonStyle="small-button" text="5" />
-        <NumberButton buttonStyle="small-button" text="6" />
-        <NumberButton buttonStyle="small-button" text="1" />
-        <NumberButton buttonStyle="small-button" text="2" />
-        <NumberButton buttonStyle="small-button" text="3" />
+        <NumberButton buttonStyle="small-number-button" text="7" />
+        <NumberButton buttonStyle="small-number-button" text="8" />
+        <NumberButton buttonStyle="small-number-button" text="9" />
+        <NumberButton buttonStyle="small-number-button" text="4" />
+        <NumberButton buttonStyle="small-number-button" text="5" />
+        <NumberButton buttonStyle="small-number-button" text="6" />
+        <NumberButton buttonStyle="small-number-button" text="1" />
+        <NumberButton buttonStyle="small-number-button" text="2" />
+        <NumberButton buttonStyle="small-number-button" text="3" />
         <NumberButton buttonStyle="large-button" text="0" />
       </div>
       <div className="operators">
@@ -27,6 +28,7 @@ const App = () => {
         <ActionButton buttonStyle="small-button" text="&#8722;" />
         <ActionButton buttonStyle="small-button" text="&#43;" />
         <ActionButton buttonStyle="small-button" text="&#61;" />
+      </div>
       </div>
     </div>
   );
